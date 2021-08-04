39d2c91c43cc57a699fcbeced5fbea73ec523afe
zbcoding <uszagb@gmail.com>
2021-08-03 20:49:38 -0500

    Initial commit

---
+++ BookContract.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: GPL-3.0
+//not functional right now, in progress
+
+pragma solidity ^0.8.0; 
+
+import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC721/IERC721.sol";
+import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC721/IERC721Receiver.sol";
+import "github.com/OpenZeppelin/openzeppelin-contracts/master/contracts/token/ERC721/ERC721.sol";
+
+mapping (uint => Book) books; //map book_id to Book
+mapping (uint => Edition) editions;
+
+
+struct Book {
+    string title;
+    address authorAddr;
+    uint book_id;
+    struct Edition[] editions;
+}
+
+struct Edition {
+    private uint _numMinted;
+}
+
+contract BookCreation is IERC721 { //implement the IERC721 interface
+    function mintBook(uint _bookID, uint _editionID) external onlyBookAuthor(_bookID) {
+        _tokenIds = _tokenIds + 1;
+        books[_bookID]._numMinted = books[_bookID]._numMinted + 1;
+        books[_bookID].editions[_editionID]._numMinted = books[_bookID].editions[_editionID]._numMinted + 1;
+
+        emit BookMinted(_tokenIds, _editionID, _bookID, books[_bookID].authorID);
+        _safeMint(msg.sender, _tokenIds);
+    }
+     function ownerOf(uint tokenID) public view virtual override returns (address) {
+        return super.ownerOf(tokenID);
+    } //override the ERC721 method ownerOf
+    
+    modifier onlyBookAuthor(uint _bookID) {
+        require(books[_bookID].authorAddr == msg.sender)
+        _;
+    }
+    _safeMint(address _authorAddr, uint _tokenIds) internal {
+        _mint(address(this), _tokenIds);
+        safeTransferFrom(address(this), _authorAddr, _tokenIds)
+    }
+}
